7eb590a6587838b665829e885f7292fc0aca6207
Michael Cabalin <mike.cabalin@gmail.com>
2024-09-17 20:51:57 +0800

    Update aeon-recon.sh

--- aeon-recon.sh
+++ aeon-recon.sh
@@ -4,36 +4,72 @@
 show_menu() {
     clear
     echo "==============================="
-    echo "          MAIN MENU"
+    echo "      Aeon-Recon MAIN MENU     "
     echo "==============================="
-    echo "1. Run Nmap Vulnerability Scan"
-    echo "2. Run CTFR SSL Fetch"
-    echo "3. Exit"
+    echo "1. Check Domain SSL"
+    echo "2. Run Masscan"
+    echo "3. Run Masscan with Target File"
+    echo "4. Check Nmap Vulnerabilities"
+    echo "5. Run Nmap HTTP Enumeration"
+    echo "6. Resolve IP to DNS with Nmap"
+    echo "7. Exit"
     echo
 }
 
-# Function to run the Nmap vulnerability scan script
-run_nmap_vuln_scan() {
-    ./nmap_vuln_scan.sh
+# Function to run the Check Domain SSL script
+run_check_domain_ssl() {
+    ./check_domain_ssl.sh
 }
 
-# Function to run the CTFR SSL fetch script
-run_ctfr_ssl() {
-    ./ctfr_domain_ssl.sh
+# Function to run the Masscan script
+run_masscan() {
+    ./masscan.sh
+}
+
+# Function to run the Masscan with Target File script
+run_masscan_target() {
+    ./masscan_target.sh
+}
+
+# Function to run the Nmap Check Vulnerabilities script
+run_nmap_checkvuln() {
+    ./nmap_checkvuln.sh
+}
+
+# Function to run the Nmap HTTP Enumeration script
+run_nmap_httpenum() {
+    ./nmap_httpenum.sh
+}
+
+# Function to run the Nmap IP to DNS script
+run_nmap_iptodns() {
+    ./nmap_iptodns.sh
 }
 
 # Main loop
 while true; do
     show_menu
-    read -p "Please select an option [1-3]: " choice
+    read -p "Please select an option [1-7]: " choice
     case $choice in
         1)
-            run_nmap_vuln_scan
+            run_check_domain_ssl
             ;;
         2)
-            run_ctfr_ssl
+            run_masscan
             ;;
         3)
+            run_masscan_target
+            ;;
+        4)
+            run_nmap_checkvuln
+            ;;
+        5)
+            run_nmap_httpenum
+            ;;
+        6)
+            run_nmap_iptodns
+            ;;
+        7)
             echo "Exiting..."
             exit 0
             ;;
